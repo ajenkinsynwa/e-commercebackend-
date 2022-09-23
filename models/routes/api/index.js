d58c02b348f097f52e8bb13f89d89c0cdc9d7f6e
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-09-22 20:55:44 -0400

    uploaded code to routes

---
+++ models/routes/api/index.js
@@ -0,0 +1,11 @@
+//Starter code provided by course 
+const router = require('express').Router();
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
+
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
+
+module.exports = router;
---
+++ models/routes/index.js
@@ -0,0 +1,11 @@
+//Starter code provided by Course 
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>")
+});
+
+module.exports = router;
